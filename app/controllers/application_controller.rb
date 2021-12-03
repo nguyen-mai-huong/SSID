beb568a325a151529fa822c3a03c84e6118d5fad
maihuong <nguyenmaihuong9009@gmail.com>
2021-12-03 22:25:11 +0700

    fix small bugs to launch app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
     def authorize
       # get user and respective membership
       @user = User.find_by_id(session[:user_id]) 
-      @membership = UserCourseMembership.find_by_user_id(@user.id)
+      # @membership = UserCourseMembership.find_by_user_id(@user.id)
       
       unless @user
         redirect_to login_url, notice: "Please log in"
